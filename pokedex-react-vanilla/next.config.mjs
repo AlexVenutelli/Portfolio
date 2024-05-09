15cc53e9a03831128eed40d540a0a315ef9798c6
Alex Venutelli <148492102+AlexVenutelli@users.noreply.github.com>
2024-05-09 11:33:57 +0200

    Delete pokedex-react-vanilla directory

--- pokedex-react-vanilla/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["pngimg.com", "i.redd.it"],
-  },
-};
-
-export default nextConfig;
--- pokedex-react-vanilla/src/pages/mainLayout/MainLayout.jsx
+++
@@ -1,3 +0,0 @@
-import MainLayout from "./index";
-
-export default MainLayout;
